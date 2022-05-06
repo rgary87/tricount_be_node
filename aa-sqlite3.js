1bbf16b55c1a9e8c9ccd930323c65b7d3006817c
rgary <gary.remy@gmail.com>
2022-05-06 13:10:06 +0200

    fix potential issue on aa-sqlite

--- aa-sqlite3.js
+++ aa-sqlite3.js
@@ -1,10 +1,10 @@
 const sqlite3 = require('sqlite3').verbose()
-var db
+let db
 
 exports.db = db
 
 exports.open=function(path) {
-    return new Promise(function(resolve) {
+    return new Promise(function(resolve, reject) {
         this.db = new sqlite3.Database(path,
             function(err) {
                 if(err) reject("Open error: "+ err.message)
@@ -40,7 +40,7 @@ exports.get=function(query, params) {
 // set of rows read
 exports.all=function(query, params) {
     return new Promise(function(resolve, reject) {
-        if(params == undefined) params=[]
+        if(params === undefined) params=[]
 
         this.db.all(query, params, function(err, rows)  {
             if(err) reject("Read error: " + err.message)
@@ -54,7 +54,7 @@ exports.all=function(query, params) {
 // each row returned one by one
 exports.each=function(query, params, action) {
     return new Promise(function(resolve, reject) {
-        var db = this.db
+        let db = this.db
         db.serialize(function() {
             db.each(query, params, function(err, row)  {
                 if(err) reject("Read error: " + err.message)
